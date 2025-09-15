e836f9ba11106c53157aa64174a45a3c59f13640
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-09-14 19:58:48 -0600

    Initial Chess Setup

--- chessboard.py
+++ chessboard.py
@@ -1,1 +1,16 @@
 import sys, copy
+
+# ----------------------------------------
+# Initial Chess Setup
+# ----------------------------------------
+# Dictionary mapping board positions (e.g., 'a8', 'b7') to chess pieces.
+# Piece codes:
+#   - First letter: color ('w' = white, 'b' = black)
+#   - Second letter: piece ('P' = Pawn, 'N' = Knight, 'B' = Bishop, 'R' = Rook, 'Q' = Queen, 'K' = King)
+
+STARTING_PIECES = {'a8': 'bR', 'b8': 'bN', 'c8': 'bB', 'd8': 'bQ',
+'e8': 'bK', 'f8': 'bB', 'g8': 'bN', 'h8': 'bR', 'a7': 'bP', 'b7': 'bP',
+'c7': 'bP', 'd7': 'bP', 'e7': 'bP', 'f7': 'bP', 'g7': 'bP', 'h7': 'bP',
+'a1': 'wR', 'b1': 'wN', 'c1': 'wB', 'd1': 'wQ', 'e1': 'wK', 'f1': 'ww',
+'g1': 'wN', 'h1': 'wR', 'a2': 'wP', 'b2': 'wP', 'c2': 'wP', 'd2': 'wP',
+'e2': 'wP', 'f2': 'wP', 'g2': 'wP', 'h2': 'wP'}
